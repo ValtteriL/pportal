c0d06c45c352e75e38e1c51a91ebb43bcd4f69e8
Valtteri <theqbot94@gmail.com>
2016-09-11 14:50:00 +0300

    added file from vm

---
+++ vm/fetchjson.php
@@ -0,0 +1,49 @@
+<?php
+include_once 'db-connect.php';
+
+
+/* input should be coordinates */
+if(isset($_GET['lo'], $_GET['la']))
+{
+    $longitude = filter_input(INPUT_GET, 'lo', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $latitude = filter_input(INPUT_GET, 'la', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+}
+else
+{
+    die('illegal arguments');
+}
+
+
+/* connect to database */
+if ($stmt = $mysqli->prepare("SELECT id, nimi, kuva, arvosana, hinta,
+            (((acos(sin((?*pi()/180)) * 
+            sin((`Latitude`*pi()/180))+cos((?*pi()/180)) * 
+            cos((`Latitude`*pi()/180)) * cos(((?- `Longitude`)* 
+            pi()/180))))*180/pi())*60*1.1515*1609.344
+            -sade
+    ) as distance
+    FROM pukit
+    HAVING distance <= 0"))
+{
+    /* fetch results - should we request only suitable ones somehow? YES if possible */
+    $stmt->bind_param('ddd', $latitude, $latitude, $longitude);
+    $stmt->execute();   // Execute the prepared query.
+
+    
+    /* generate JSON of the suitable ones */
+    $array_of_pukkis = array();
+    while($r = mysqli_fetch_assoc($stmt))
+    {
+         $array_of_pukkis[] = $r;
+    }
+
+    /* print out the json */
+    print json_encode($array_of_pukkis);
+}
+else
+{
+    die('database connectionz failez');
+}
+
+
+?>
