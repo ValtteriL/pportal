d4b53bee604fd4cdcc7fee51a03f6f8046896afd
Valtteri <theqbot94@gmail.com>
2016-09-11 16:14:34 +0300

    Cleaned things up a bit + improved init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -10,21 +10,31 @@ CREATE TABLE pportal.pukit (
     latitude DECIMAL(10,8) NOT NULL,
     longitude DECIMAL(11,8) NOT NULL,
     radius INT NOT NULL,
-    description VARCHAR(140),
+    description VARCHAR(500),
     image CHAR(4),
     password CHAR(128) NOT NULL,
     rating INT,
+    suomi BOOLEAN,
+    ruotsi BOOLEAN,
+    englanti BOOLEAN,
+    venaja BOOLEAN,
     PRIMARY KEY(id)
 );
 
 
 CREATE TABLE pportal.customers (
+    id INT NOT NULL AUTO_INCREMENT,
+    idpukki INT NOT NULL,
     firstname VARCHAR(15) NOT NULL,
     lastname VARCHAR(15) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    puh VARCHAR(10) NOT NULL
+    rating INT,
+    kommentti VARCHAR(300),
+    puh VARCHAR(10) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 
 GRANT SELECT, INSERT, UPDATE ON pportal.pukit TO 'testuser'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON pportal.customers TO 'testuser'@'localhost';
 FLUSH PRIVILEGES;
