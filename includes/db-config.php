a6c7597f79ba8c2f87677117b9d742cbb8d0675e
Valtteri <theqbot94@gmail.com>
2016-10-10 22:15:32 +0300

    Cleaned up a bit

--- includes/db-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Database credentials */
-define("HOST", "localhost");
-define("USER", "testuser");
-define("PASSWORD", "example");
-define("DATABASE", "pportal");
-
-?>
--- includes/db-connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once 'db-config.php';
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
